ba0e1a2146384b4059d500c4f1e94773b1fb2e7b
Ds Esteban <dennis.esteban.89@gmail.com>
2015-03-07 12:36:04 -0430

    Primer Commit. archivo.c - Carla

---
+++ archivo.c
@@ -0,0 +1,68 @@
+
+/*
+ * CI4835 Redes de Computadoras I
+ * Proyecto: Sistema de Chat Simple (SCS)
+ * Archivo:
+ * Descripción:
+ *
+ * Integrantes:  
+ *      07-41329 Dennis Pérez
+ *		08-10792 Carla Nardone
+ *      09-10381 Christhian Guevara
+ *		09-10676 Rebecca Porras
+ */
+ 
+ 
+ ///////////////////////////////////
+ //   Comandos No Privilegiados   //
+ ///////////////////////////////////
+ 
+ // conectarse - Hace un login al SCS
+ 
+ // salir - Ejecuta un logout del SCS
+ 
+ // entrar - Entra a una sala activa en el SCS
+ 
+ // dejar - Sale de una sala activa en el SCS en la cual se estaba presente
+ 
+ // ver_salas - Muestra la lista de salas activas en el SCS
+ 
+ // ver_usuarios - Muestra la lista de usuarios declarados en el SCS
+ 
+ // ver_usu_salas - Muestra la lista actual de usuarios suscritos a una sala
+ 
+ // env_mensaje - Envía un mensaje de texto por difusión a una sala en la cual se está presente
+ 
+  
+ 
+ ///////////////////////////////////
+ //    Comandos Privilegiados     //
+ ///////////////////////////////////
+ 
+ // crear_usu - Crea el soporte de datos para habilitar una cuenta de un usuario
+ 
+ // elim_usu - Elimina una cuenta de usuario. Si el usuario está en línea, deberá sacarlo forzadamente 
+ //            del sistema, sin opción a que ingrese nuevamente
+ 
+ // crear_sala - Crea una sala y la hace activa en el SCS
+ 
+ // OJO: Uhm, esto el profe lo redactó mal, asumo que es que si hay usuarios se deben sacar de la sala
+ // elim_sala - Elimina una sala activa en el SCS. Si en la sala están presentes algunos usuarios ...
+ 
+ // ver_sala - Muestra la lista de salas activas en el SCS
+ 
+ // ver_usuarios - Muestra la lista de usuarios suscritos a una sala
+ 
+ // hab_sala - Activa una sala para que pueda prestar servicios
+ 
+ // deshab_sala - Desactiva una sala para que pueda prestar servicios
+ 
+ // ver_log - Muestra el contenido de la bitácora completa. La bitácora graba todos los sucesos que 
+ //           suceden desde que el SCS se activa hasta que se apaga. Acumula el contenido añadiendo
+ //           hechos, operaciones, mensajes privados por difusión, etc.
+ 
+ 
+ 
+ 
+ 
+ 
